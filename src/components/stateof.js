0424a3ef1825d64b9dc31762d6adcf14556bc790
Alexander Chi <arturochi2@hotmail.com>
2021-08-25 19:33:07 -0500

    Se agregó al components/stateof.js estructura de componente Se agregó css para stateof.js component

--- src/components/stateof.js
+++ src/components/stateof.js
@@ -4,13 +4,21 @@
  * @date: 11/Agosto/2021
 **/
 import React, { useState } from 'react';    
-
+import mobile from '../assets/svgs/illustration-phones.svg' 
 export function StateOf(props) {
-    const {name} = props.options;
+    const {name, title, content} = props.options;
  
-    return (<section className={"section_" + name} >
- 
-    </section>);
+    return (
+        <section className={"section_" + name} >
+            <div className="state__content state__img">
+                <img alt={title} src={mobile} />
+            </div> 
+            <div className="state__content state__text content__center__vertical">
+                <h3 className="content__title">{title}</h3>
+                <p className="content__text">{content}</p>
+            </div>
+        </section>
+    );
 }
   
 export default StateOf;
