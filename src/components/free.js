b699e8073b31e16f1f993c5cb68dd84179b96548
Alexander Chi <arturochi2@hotmail.com>
2021-08-25 22:43:58 -0500

    Se agregó estructura del components/free.js Se agregó css para components/free.js Se agregó ajustes en rangos de resoluciones para el responsive

--- src/components/free.js
+++ src/components/free.js
@@ -4,13 +4,33 @@
  * @date: 11/Agosto/2021
 **/
 import React, { useState } from 'react';    
+import illustration from '../assets/svgs/illustration-laptop-desktop.svg';
 
 export function Free(props) {
-    const {name} = props.options;
+    const {name, content} = props.options;
  
-    return (<section className={"section_" + name} >
- 
-    </section>);
+    return (
+        <section className={"section_" + name} >
+            <div className="container"> 
+                <div className="content content__l">
+                    <img alt="Free" src={illustration}></img>
+                </div>
+                <div className="content content__center__vertical">
+                    {content.map((item, index) => {
+                        return (<div key={item.title.toLowerCase()} className="content__info">
+                            <h3 className="content__title">
+                                {item.title}
+                            </h3>
+                            <p className="content__text">
+                                {item.text}
+                            </p>
+                        </div>)
+                    })}
+                </div> 
+                
+            </div>
+        </section>
+    );
 }
   
 export default Free;
