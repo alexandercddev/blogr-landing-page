2cd6dad0fea70bec62765ba2cc36b263dc10155d
Alexander Chi <arturochi2@hotmail.com>
2021-08-28 02:05:29 -0500

    Se agregó ajustes al JSX para usar el mismo menu en movil como en escritorio Se agregó css

--- src/components/header.js
+++ src/components/header.js
@@ -5,12 +5,13 @@
 **/
 import React, { useState } from 'react';    
 
-
 export function Header(props) {
     const [className, setClaseName] = useState('');
+    const [active, setActive] = useState('');
     const { options, logo } = props;
     const handleClose = (event) => {
         setClaseName(className === '' ? 'header__close' : '')
+        setActive(active === '' ? 'active' : '')
     }
     return (
         <header className="header__container" >
@@ -19,27 +20,32 @@ export function Header(props) {
                     <div className="header__div__img">
                         <img alt="Blogr" className="logo__img" src={logo}></img>
                     </div>
-                    <ul className="ul__option__desktop">
-                        {options.main.options.map((item, index) => {
-                            return (<li key={item.key} className={"li__option__desktop " + item.key} >
-                                <a href={"#" +item.key} className="a__option__desktop">{item.text}</a>
-                            </li>)
-                        })}
-                    </ul>
-                </div>
-                <div className="header_div__button">
-                    <ul className="ul__button__desktop">
-                        {options.main.buttons.map((item, index) => {
-                            return (<li key={item.key} className={"li__button__desktop " + item.key}>
-                                <a href={"#" +item.key} className="a__button__desktop">{item.text}</a>
-                            </li>)
-                        })}
-                    </ul>
-                </div>
-                <div 
-                    className={`header__menu ${className}`} 
-                    onClick={handleClose}>
-                </div>
+                    <div className={`header__content__menu ${active}`}>
+                        <ul className="ul__option__desktop">
+                            {options.main.options.map((item, index) => {
+                                return (<li key={item.key} className={"li__option__desktop " + item.key} >
+                                    <a href={"#" +item.key} className="a__option__desktop">{item.text}</a>
+                                </li>)
+                            })}
+                        </ul>
+                        <div className="header_div__button">
+                            <ul className="ul__button__desktop">
+                                {options.main.buttons.map((item, index) => {
+                                    return (<li key={item.key} className={"li__button__desktop " + item.key}>
+                                        <a href={"#" +item.key} className="a__button__desktop">{item.text}</a>
+                                    </li>)
+                                })}
+                            </ul>
+                        </div> 
+                    </div>
+                    
+                    <div className="header_div__menu">
+                        <div 
+                            className={`header__menu ${className}`} 
+                            onClick={handleClose}>
+                        </div>
+                    </div> 
+                </div> 
             </div>
             <div className="header_div__content">
                 <div className="header_div__titles">
